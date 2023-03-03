f46a429e6d6f101cf4dd6683ab11cdc82383963f
Madelen Illanes <98763869+madelen-illanes@users.noreply.github.com>
2023-03-03 16:57:12 -0500

    second commit

--- components/emojiCard.tsx
+++ components/emojiCard.tsx
@@ -1,18 +1,39 @@
-import * as React from 'react';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+import React from "react";
+
+const inter = Inter({ subsets: ['latin'] })
 
 export interface EmojiCardProps {
 
-    code: 'string',
-    emoji: 'string',
-    name: 'string',
-    category: 'string',
-    group: 'string',
-    subgroup: 'string',
+    code?: string,
+    emoji: string,
+    name: string,
+    category?: string,
+    group?: string,
+    subgroup?: string,
 
 }
-const EmojiCard:React.FunctionComponent<EmojiCardProps> =() => {
+const EmojiCard:React.FunctionComponent<EmojiCardProps> =({
+                                                              code,
+                                                              emoji,
+                                                              name,
+                                                              category,
+                                                              group,
+                                                              subgroup
+                                                          }) => {
     return(
-        <>
-        </>
+        <React.Fragment>
+            <div className='bg-[#F7F6F3] p-2 rounded-xl shadow-md flex flex-col text-center'>
+                <span className='text-4xl md:text-5xl mb-2'role={'img'} aria-label={emoji}>{emoji}</span>
+                <h3 className='font-bold text-gray-700' >{name}</h3>
+<button className='text-blue-500 text text-sm pt-3'>
+    more info
+</button>
+            </div>
+
+        </React.Fragment>
     )
 }
+export default EmojiCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,29 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import RootLayout from "@/app/layout";
+import EmojiCard from "../../components/emojiCard";
+import List from "../../components/list";
+import emojisData from "../../data/emojis (1)";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
 
-      // eslint-disable-next-line react/jsx-no-undef
+    return (
+
       <RootLayout>
-        <h1 className='text-red-500'>HELLO</h1>
+          <h1 className='text-2xl md:text-5xl fond-bold text-gray-600 text-center tracking-wide'>Welcome to Infoji</h1>
+          <div>
+              <input
+              className='w-full max-w-xl border border-gray-300 p-3 rounded-md my-8 bg-white shadow-md focus:outline-none focus:border-cyan-500 focus:ring-1'
+              id='name'
+              type={'text'}
+              placeholder='Search for emoji'
+              />
+          </div>
+        <List emojis={emojisData.slice(0, 100)}
+        />
+
       </RootLayout>
 
   )
